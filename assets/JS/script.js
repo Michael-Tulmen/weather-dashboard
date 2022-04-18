6584e00b5a1f53427aba3862b36bd35b569da871
Michael Tulmen <michaeltulmen@gmail.com>
2022-04-17 21:27:13 -0500

    API connection works now

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,20 @@
+//css framework using tailwind
+tailwind.config = {
+    theme: {
+      extend: {
+          fontFamily: {
+              sans: ['Josefin Slab', 'serif']
+          }
+      }
+    }
+}
+
 // let city equal whatever is in the input field
 let city = "";
 
 //other variables
-let searchedCity = "#searched-city"
-let searchButton = "#search-button"
+let inputValue = document.querySelector(".inputValue")
+let button = document.querySelector('#search-button')
 let clearSearch = '#clear-button'
 let currentCity = "#current-city"
 let cityTemp = "#city-temperature"
@@ -11,6 +22,7 @@ let cityHum = "#city-humidity"
 let cityWind = "#city-wind-speed"
 let cityUV = "#city-UV-index"
 let cityArray = [];
+
 
 
 // does the city that the user searches already exist within localstorage, or do we have to work again
@@ -23,67 +35,38 @@ function find(city) {
     }
     return 1;
 }
-//where the program will get my eventual API key
-let APIKey = "bc187066e8df862ebc540da9d166d0a3"
 
-//get current and future weather in the form of forecast after searching for city
-function getWeather(event) {
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=bc187066e8df862ebc540da9d166d0a3')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => alert("Incorrect City Entered, Try Again!"))
+})
+
+//clear code
+function clearSearches(event){
     event.preventDefault();
-    if(searchedCity.valueOf()!==""){
-        city=searchedCity.val();
+    cityArray=[];
+    localStorage.removeItem("cityname")
+    document.location.reload();
+}
+
+
+function loadLastCity(){
+    $("ul").empty();
+    var cityArray = JSON.parse(localStorage.getItem(cityname));
+    if(cityArray!==null){
+        cityArray=JSON.parse(localStorage.getItem("cityname"));
+        for(i=o; I<cityArray.length;i++){
+            addToList(cityArray[i]);
+        }
+        city=cityArray[i-1];
         currentWeather(city);
     }
 }
 
-// Call function
-function currentWeather(city){
-    let queryURL="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
-    $.ajax({
-        url:queryURL,
-        method:"GET",
-    }).then(function(response){
+//handler code
 
-        console.log(response);
-
-        let weathericon= response.weather[0].icon;
-        let iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
-
-        let date=new Date(response.dt*1000).toLocaleDateString();
-
-
-        $(currentCity).html(response.name + "("+date+")" + "<img src=" +iconurl+">");
-        
-        //temp
-        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        $(cityTemp).html((tempF).toFixed(2)+"&#8457");
-
-        //hum
-        $(cityHum).html(response.main.humidity+"%");
-
-        //wind speed
-        let ws=response.wind.speed;
-        let windspeedmph = (ws*2.237).toFixed(2);
-        $(cityWind).html(windsmph + "MPH");
-    })
-}
-//Get forecast
-function forecast(cityid){
-    var dayover=false;
-    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
-    $.ajax({
-        url:queryforcastURL,
-        method:"GET"
-    }).then(function(response){
-        for (i=0;i<5;i++){
-
-    })
-//css framework using tailwind
-tailwind.config = {
-    theme: {
-      extend: {
-          fontFamily: {
-              sans: ['Josefin Slab', 'serif']
-          }
-      }
-    }
-}
+$("#clear-button").on('click',clearSearches);
+$(document).on('click',getPastSearch);
+$(window).on('load',loadLastCity);
